78a81ddd25a5f6fd128014c3c1b1fad97d08f51e
Samuel Jonas <samueljonas@alu.ufc.br>
2025-02-14 11:44:40 -0300

    feat: docker setup

--- priv/repo/migrations/20250211132126_create_posts.exs
+++ priv/repo/migrations/20250211132126_create_posts.exs
@@ -2,13 +2,26 @@ defmodule Xweeter.Repo.Migrations.CreatePosts do
   use Ecto.Migration
 
   def change do
-    create table(:posts) do
-      add :username, :string
-      add :body, :string
-      add :likes_count, :integer
-      add :reposts_count, :integer
+    unless table_exists?("posts") do
+      create table(:posts) do
+        add :username, :string
+        add :body, :string
+        add :likes_count, :integer
+        add :reposts_count, :integer
 
-      timestamps(type: :utc_datetime)
+        timestamps(type: :utc_datetime)
+      end
     end
   end
+
+  defp table_exists?(table_name) do
+    query = "SELECT to_regclass('public.#{table_name}')"
+    Ecto.Adapters.SQL.query!(Xweeter.Repo, query, [])
+    |> Map.get(:rows)
+    |> case do
+      [[nil]] -> false
+      _ -> true
+    end
+  end
+
 end
